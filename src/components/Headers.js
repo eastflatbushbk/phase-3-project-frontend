221b5d0ce02332a26b2e385a2266b21d2d3188bd
Robert Sainton <robertsainton@outlook.com>
2023-01-22 20:50:04 -0500

    create Headers function

---
+++ src/components/Headers.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Header } from "semantic-ui-react";
+
+function Headers() {
+  return (
+    
+    <div>
+      <h1>Sunday Rec League</h1>
+    </div>
+    
+  );
+}
+
+export default Headers;
