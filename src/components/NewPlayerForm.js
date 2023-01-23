4c77e82653e7282e5bcbe68c363a9da667cb9b7c
Robert Sainton <robertsainton@outlook.com>
2023-01-22 21:56:11 -0500

    code  NewPlayerForm jsx

--- src/components/NewPlayerForm.js
+++ src/components/NewPlayerForm.js
@@ -5,5 +5,31 @@ const defaultData = {
     name: "",
     position: "",
     status: ""
-    
+      }
+  
+    function NewPlayerForm() {
+
+        return (
+            <div class="container">  
+          <form id="contact"  onSubmit={handleSubmit}>
+            <h8>ADD NEW PLAYER</h8>
+            <h9>type new player info below</h9>
+            <fieldset>
+              <input placeholder="Player Name" value={formData.name} onChange={handleChange} type="text" tabindex="1" name="name" />
+            </fieldset>
+            <fieldset>
+              <input placeholder="Position" type="text" tabindex="2" required autofocus value={formData.position} onChange={handleChange} name="position" />
+            </fieldset>
+            <fieldset>
+              <input placeholder="Status" type="text" tabindex="3" required autofocus value={formData.status} onChange={handleChange} name="status" />
+            </fieldset>
+            
+            <fieldset>
+              <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+            </fieldset>
+          </form>
+         
+          
+        </div>
+          )
     }
