e669976d78fc3cc97b9c829c961593668a251f96
Robert Sainton <robertsainton@outlook.com>
2023-01-22 21:06:58 -0500

    set state , params and Navigate

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -3,3 +3,15 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 // import PlayerList from './PlayerList'
 import PlayerCard from './PlayerCard'
+
+const TeamCard = () => {
+    const [team, setTeam] = useState({players: []})
+   const params = useParams()
+   const navigate = useNavigate();
+
+
+
+
+
+
+}
